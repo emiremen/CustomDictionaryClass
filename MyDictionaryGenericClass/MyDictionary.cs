dcea9c8d512af2426805031d460876a6d8daed3d
Muhammed EMEN <emiremen001@gmail.com>
2022-06-08 22:20:02 +0300

    first commit

---
+++ MyDictionaryGenericClass/MyDictionary.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionaryGenericClass
+{
+    public class MyDictionary<TKey, TValue>
+    {
+        private TKey[] _arrayKey;
+        private TValue[] _arrayValue;
+
+
+        public MyDictionary()
+        {
+            _arrayKey = new TKey[0];
+            _arrayValue = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            var tempArrayKey = _arrayKey;
+            _arrayKey = new TKey[_arrayKey.Length + 1];
+
+            var tempArrayValue = _arrayValue;
+            _arrayValue = new TValue[_arrayValue.Length + 1];
+
+            for (int i = 0; i < tempArrayKey.Length; i++)
+            {
+                _arrayKey[i] = tempArrayKey[i];
+                _arrayValue[i] = tempArrayValue[i];
+            }
+
+            _arrayKey[_arrayKey.Length - 1] = key;
+            _arrayValue[_arrayValue.Length - 1] = value;
+        }
+
+        public object this[TKey key]
+        {
+            get
+            {
+                TValue _key = default(TValue);
+                for(int i =0; i < _arrayKey.Length;i++)
+                {
+                    if (Equals(key, _arrayKey[i]))
+                    {
+                        _key = _arrayValue[i];
+                        break;
+                    }
+                }
+                return _key;
+            }
+        }
+    }
+}
---
+++ MyDictionaryGenericClass/Program.cs
@@ -0,0 +1,12 @@
+﻿using MyDictionaryGenericClass;
+
+MyDictionary<string,int> asd = new MyDictionary<string, int>();
+asd.Add("as", 777);
+
+Console.WriteLine(asd["as"]);
+
+
+MyDictionary<int, int> asdd = new MyDictionary<int, int>();
+asdd.Add(57, 333);
+
+Console.WriteLine(asdd[57]);
